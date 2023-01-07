dee3e87a9cfff080af8d80e5ffd0900ccff9d952
Svitlana D <svetk2801@gmail.com>
2023-01-07 22:57:09 +0200

    module 4 task 1

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        if (a > b) {
+            System.out.println("false");
+        } else {
+            System.out.println("false");
+        }
     }
 }
